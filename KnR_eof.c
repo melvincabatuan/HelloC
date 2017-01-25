d88bf1c6df4107172ffc38ff56ffc7d3b50f7114
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-01-25 23:32:36 +0800

    Create KnR_eof.c

---
+++ KnR_eof.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int input;
+	puts("Input a character or Ctrl+z to signal EOF");
+	while ((input = getchar()) != EOF) {
+		printf("The value of getchar() != EOF is %d.\n", 
+			(input != EOF));
+		getchar();
+	}
+	printf("The value of getchar() != EOF is %d.\n", 
+		(input != EOF));
+	system("pause");
+	return 0;
+}
