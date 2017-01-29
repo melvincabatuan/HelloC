bbdaf7528408c22b627a0824151ca400a7a615a6
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-01-29 23:37:10 +0800

    Create DeTab.c

---
+++ DeTab.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAB 8
+
+int main()
+{
+	int input;
+	int counter = 0;
+	int spaces;
+	while ((input = getchar()) != EOF) {
+		if (input == '\n') {
+			putchar(input);
+			counter = 0;
+		}
+		else if (input == '\t') {
+			spaces = TAB - counter%TAB;
+			while (spaces-- != 0) {
+				putchar('#');
+				counter++;
+			}
+		}
+		else {
+			putchar(input);
+			counter++;
+		}
+	}
+	system("pause");
+	return 0;
+}
