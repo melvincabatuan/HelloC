ccd1f2c8d00f74a7bb6404b5e317362e2894a484
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-01-30 15:17:18 +0800

    Create GradeCheck.c

---
+++ GradeCheck.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	float M1, M2, M3, M4, GRADE;
+	printf("Enter 4 integers: ");
+	scanf("%f %f %f %f", &M1, &M2, &M3, &M4);
+	GRADE = (M1 + M2 + M3 + M4)/4.0;
+	if(GRADE < 70)
+		printf("FAIL\n");
+	else
+		printf("PASS\n");
+	system("pause");
+	return 0;
+}
