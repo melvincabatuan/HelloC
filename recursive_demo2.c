6ff1c0ddeb505b4bbe630703489e174b9e11cfaa
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-02-01 07:31:52 +0800

    Create recursive_demo2.c

---
+++ recursive_demo2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pow(int a, int n) {
+	if (n == 0)
+		return 1;
+	else
+		return (a*pow(a, n-1));
+}
+
+int main()
+{
+	int a = 2;
+	printf("f(%d, 1) = %d\n", a, pow(a, 1));
+	printf("f(%d, 2) = %d\n", a, pow(a, 2));
+	printf("f(%d, 3) = %d\n", a, pow(a, 3));
+	printf("f(%d, 4) = %d\n", a, pow(a, 4));
+	system("pause");
+	return 0;
+}
