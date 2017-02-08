ded97aab62713d288bb5ef18be12f06a7c8b8cb4
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-02-08 20:26:59 +0800

    Create factorial_recursion.c

---
+++ factorial_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(int n) {
+	if (n == 0)
+		return 1;
+	else
+		return (n*f(n-1));
+}
+
+int main()
+{
+	printf("f(1) = %d\n", f(1));
+	printf("f(2) = %d\n", f(2));
+	printf("f(3) = %d\n", f(3));
+	printf("f(4) = %d\n", f(4));
+	system("pause");
+	return 0;
+}
