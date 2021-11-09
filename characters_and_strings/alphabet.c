bdf1e9ed6eaabcaba1134fb73d823f0de932a830
melvincabatuan <melvincabatuan@gmail.com>
2021-11-10 03:06:59 +0800

    reverted alphabet ex to original

--- characters_and_strings/alphabet.c
+++ characters_and_strings/alphabet.c
@@ -2,19 +2,17 @@
 
 int main()
 {
-  char chr;
-  printf("\n");
-
-  chr = 'a';
-  char encoded = 'a' + ((chr + 3) - 'a') % 26;
-  printf("encode char = %c", encoded); 
-
-  // for (chr = 'a'; chr <= 'z'; chr = chr + 1)
-  // {
-  //   printf("%c ", chr);
-  // }
+  for (char chr = 'a'; chr <= 'z'; chr = chr + 1)
+  {
+    printf("%c ", chr);
+  }
   // for (chr = 'A'; chr <= 'Z'; chr = chr + 1) {
   //   printf("%c ", chr);
   // }
+
+  // Encoding demo - lab1 ex2 example:
+  // char chr = 'a';
+  // char encoded = 'a' + ((chr + 3) - 'a') % 26;
+  // printf("encode char = %c", encoded);
   return 0;
 }
