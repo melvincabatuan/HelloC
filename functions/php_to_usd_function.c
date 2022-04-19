386e94c167f6fefa54de8ac25356dd07f30c55b4
melvincabatuan <melvincabatuan@gmail.com>
2022-04-19 12:11:30 +0800

    updated format of php2usd conversion

--- functions/php_to_usd_function.c
+++ functions/php_to_usd_function.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-// DEFINITION:
-// return_type function_name(INPUT_PARAMETERS)
-float php2usd(float php){
-    float usd = php / 52.368361f;
-    printf("%0.2f PHP = %0.2f USD\n", php, usd);
-}
+float php2usd(float php);
 
-int main(){
+int main()
+{
     php2usd(1000); // CALL
     return 0;
+}
+
+// DEFINITION:
+// return_type function_name(INPUT_PARAMETERS)
+float php2usd(float php)
+{
+    float usd = php / 52.368361f;
+    printf("%0.2f PHP = %0.2f USD\n", php, usd);
 }
