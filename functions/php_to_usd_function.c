a5e310a71d7f404477a457e8d2b718ffc6cee41a
melvincabatuan <melvincabatuan@gmail.com>
2022-04-19 12:22:19 +0800

    updated global constant for php2usd conversion

--- functions/php_to_usd_function.c
+++ functions/php_to_usd_function.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+// GLOBAL CONSTANTS: usual C practice
+#define X_PHPUSD 52.368361f
+
 float php2usd(float php);
 
 int main()
 {
-    php2usd(1000); // CALL
+    float my_pera;
+    my_pera = 1000000.0f;
+    php2usd(my_pera); // CALL
     return 0;
 }
 
@@ -12,6 +17,6 @@ int main()
 // return_type function_name(INPUT_PARAMETERS)
 float php2usd(float php)
 {
-    float usd = php / 52.368361f;
+    float usd = php / X_PHPUSD;
     printf("%0.2f PHP = %0.2f USD\n", php, usd);
 }
