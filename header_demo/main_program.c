1d967be1895319969eb6b1de67ee953e2badc959
melvincabatuan <melvincabatuan@gmail.com>
2022-05-19 11:35:22 +0800

    addfed basic header demo

---
+++ header_demo/main_program.c
@@ -0,0 +1,9 @@
+// #include <stdio.h> // possible to exclude due to header inclusion
+#include "my_header.h" // Custom header
+
+int main()
+{
+    greet();
+    printf("\n 3 + 5 = %d", sum_of_two(3,5));
+    return 0;
+}
---
+++ header_demo/my_header.h
@@ -0,0 +1,13 @@
+#include <stdio.h> // Built-in header needed by printf
+
+void greet()
+{
+    printf("Hello, World!");
+    printf("\nHow are you!");
+    return;
+}
+
+int sum_of_two(int a, int b)
+{
+    return a + b;
+}
