ff352e5a6806d28d231583d4503a780876541e53
melvincabatuan <melvincabatuan@gmail.com>
2022-05-19 13:08:58 +0800

    added sample array sum function

--- header_demo/main_program.c
+++ header_demo/main_program.c
@@ -5,5 +5,9 @@ int main()
 {
     greet();
     printf("\n 3 + 5 = %d", sum_of_two(3,5));
+
+    // array sum sample
+    int scores[] = {20, 30, 40 ,50 , 60};
+    printf("\nThe sum is %d." , array_sum(scores, 5));
     return 0;
 }
--- header_demo/my_header.h
+++ header_demo/my_header.h
@@ -1,5 +1,7 @@
 #include <stdio.h> // Built-in header needed by printf
 
+#define MAXSIZE 10
+
 void greet()
 {
     printf("Hello, World!");
@@ -10,4 +12,14 @@ void greet()
 int sum_of_two(int a, int b)
 {
     return a + b;
+}
+
+int array_sum(int numbers[], int length)
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum;
 }
