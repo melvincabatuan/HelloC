93d73f3d4b9ba1831ddd16b891385a50ed533b43
melvincabatuan <melvincabatuan@gmail.com>
2022-05-24 12:12:35 +0800

    added more demo

---
+++ arrays/array_basic_demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // Declaration
+    int numbers[4];
+    // Access or assign values
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    // Display
+    printf("\nThe first number is %d.", numbers[0]);
+    printf("\nThe second number is %d.", numbers[1]);
+    printf("\nThe third number is %d.", numbers[2]);
+    printf("\nThe fourth number is %d.", numbers[3]);
+    return 0;
+}
---
+++ header_demo2/my_header.h
@@ -0,0 +1,14 @@
+#include <stdio.h> // needed by printf
+
+#define PI 3.14159
+
+void greet()
+{
+    printf("Hello, World!");
+    return;
+}
+
+int sum_of_two(int a, int b)
+{
+    return a + b;
+}
